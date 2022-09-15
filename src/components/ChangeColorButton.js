22c8606bfa1b5c7c7626e9c30266b0e31e0ea32b
Miguel C <migs.castaneda@gmail.com>
2022-09-14 21:21:18 -0400

    toggle color

--- src/components/ChangeColorButton.js
+++ src/components/ChangeColorButton.js
@@ -1,9 +1,13 @@
 import Button from 'react-bootstrap/Button';
 
+
 function ChangeColorButton() {
+  const [toggle, setToggle] = React.useState(false);
+  const toggleButton = () => setToggle(!toggle);
+
   return (
     <>
-      <Button variant="dark" size="sm">Change Background Color</Button>{' '}
+      <Button style={{backgroundColor: toggle ? 'white' : 'red'}} onClick={toggleButton} variant="dark" size="sm">Change Background Color</Button>{' '}
     </>
   );
 }
